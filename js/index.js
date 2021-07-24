4e20d2d80bbf3fee8bc31e3e56e6257d19898e44
dangdinhvan <untilyou1235@gmail.com>
2021-07-24 13:08:23 +0700

    ver 1.0

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(".carousel").slick({
+    prevArrow:
+        '<button class="arrow-slick prev-slick"><i class="fa  fa-angle-left" aria-hidden="true"></i></button>',
+    nextArrow:
+        '<button class="arrow-slick next-slick"><i class="fa  fa-angle-right caret" aria-hidden="true"></i></button>',
+    autoplay: true,
+    dots: true,
+    autoplaySpeed: 2000,
+});
---
+++ js/main.js
@@ -0,0 +1,12 @@
+$('#hamberger').on('click', function () {
+    $('#mobile-menu-container').addClass('show');
+})
+
+
+$(window).on('click', function (event) {
+    if ($(event.target).is($('#mobile-menu-container'))) {
+        $('#mobile-menu-container').removeClass('show');
+        $('input[type=checkbox]').prop('checked', false);
+        $('#menu-mobile').css('animation', 'none');
+    }
+});
