344c9acf062bf6220f422ffe5c695da2cf5fc74f
dangdinhvan <untilyou1235@gmail.com>
2021-07-30 02:52:37 +0700

    update

--- js/Laptop MSI GS65 Stealth 9SD-1409VN.js
+++ js/Laptop MSI GS65 Stealth 9SD-1409VN.js
@@ -23,65 +23,103 @@ $(".products").slick({
         '<button class="arrow-slick next-slick"><i class="fa  fa-angle-right caret" aria-hidden="true"></i></button>',
     slidesToShow: 5,
     slidesToScroll: 5,
+    responsive: [
+        {
+            breakpoint: 1025,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+    ]
 });
+
+
 // active img
 function activeImg(imgNumber) {
     if (imgNumber === '1') {
-        $('#sub-img-1').addClass('active');
-        $('#sub-img-2').removeClass('active');
-        $('#sub-img-3').removeClass('active');
-        $('#sub-img-4').removeClass('active');
-        $('#sub-img-5').removeClass('active');
-        $('#sub-img-6').removeClass('active');
-        $("#img-active").attr("src", "img/laptop-img-1.webp");
+        $('#sub-img-1').addClass('sub-img-active');
+        $('#sub-img-2').removeClass('sub-img-active');
+        $('#sub-img-3').removeClass('sub-img-active');
+        $('#sub-img-4').removeClass('sub-img-active');
+        $('#sub-img-5').removeClass('sub-img-active');
+        $('#sub-img-6').removeClass('sub-img-active');
+        $("#imageZoom").attr("src", "img/laptop-img-1.webp");
+        $('.containerZoom').css('background-image', 'url("img/laptop-img-1.webp")');
     }
     else if (imgNumber === '2') {
-        $('#sub-img-2').addClass('active');
-        $('#sub-img-1').removeClass('active');
-        $('#sub-img-3').removeClass('active');
-        $('#sub-img-4').removeClass('active');
-        $('#sub-img-5').removeClass('active');
-        $('#sub-img-6').removeClass('active');
-        $("#img-active").attr("src", "img/laptop-img-2.webp");
+        $('#sub-img-2').addClass('sub-img-active');
+        $('#sub-img-1').removeClass('sub-img-active');
+        $('#sub-img-3').removeClass('sub-img-active');
+        $('#sub-img-4').removeClass('sub-img-active');
+        $('#sub-img-5').removeClass('sub-img-active');
+        $('#sub-img-6').removeClass('sub-img-active');
+        $("#imageZoom").attr("src", "img/laptop-img-2.webp");
+        $('.containerZoom').css('background-image', 'url("img/laptop-img-2.webp")');
     }
     else if (imgNumber === '3') {
-        $('#sub-img-3').addClass('active');
-        $('#sub-img-2').removeClass('active');
-        $('#sub-img-1').removeClass('active');
-        $('#sub-img-4').removeClass('active');
-        $('#sub-img-5').removeClass('active');
-        $('#sub-img-6').removeClass('active');
-        $("#img-active").attr("src", "img/laptop-img-3.webp");
+        $('#sub-img-3').addClass('sub-img-active');
+        $('#sub-img-2').removeClass('sub-img-active');
+        $('#sub-img-1').removeClass('sub-img-active');
+        $('#sub-img-4').removeClass('sub-img-active');
+        $('#sub-img-5').removeClass('sub-img-active');
+        $('#sub-img-6').removeClass('sub-img-active');
+        $("#imageZoom").attr("src", "img/laptop-img-3.webp");
+        $('.containerZoom').css('background-image', 'url("img/laptop-img-3.webp")');
     }
     else if (imgNumber === '4') {
-        $('#sub-img-4').addClass('active');
-        $('#sub-img-2').removeClass('active');
-        $('#sub-img-3').removeClass('active');
-        $('#sub-img-1').removeClass('active');
-        $('#sub-img-5').removeClass('active');
-        $('#sub-img-6').removeClass('active');
-        $("#img-active").attr("src", "img/laptop-img-4.webp");
+        $('#sub-img-4').addClass('sub-img-active');
+        $('#sub-img-2').removeClass('sub-img-active');
+        $('#sub-img-3').removeClass('sub-img-active');
+        $('#sub-img-1').removeClass('sub-img-active');
+        $('#sub-img-5').removeClass('sub-img-active');
+        $('#sub-img-6').removeClass('sub-img-active');
+        $("#imageZoom").attr("src", "img/laptop-img-4.webp");
+        $('.containerZoom').css('background-image', 'url("img/laptop-img-4.webp")');
     }
     else if (imgNumber === '5') {
-        $('#sub-img-5').addClass('active');
-        $('#sub-img-2').removeClass('active');
-        $('#sub-img-3').removeClass('active');
-        $('#sub-img-4').removeClass('active');
-        $('#sub-img-1').removeClass('active');
-        $('#sub-img-6').removeClass('active');
-        $("#img-active").attr("src", "img/laptop-img-5.webp");
+        $('#sub-img-5').addClass('sub-img-active');
+        $('#sub-img-2').removeClass('sub-img-active');
+        $('#sub-img-3').removeClass('sub-img-active');
+        $('#sub-img-4').removeClass('sub-img-active');
+        $('#sub-img-1').removeClass('sub-img-active');
+        $('#sub-img-6').removeClass('sub-img-active');
+        $("#imageZoom").attr("src", "img/laptop-img-5.webp");
+        $('.containerZoom').css('background-image', 'url("img/laptop-img-5.webp")');
     }
     else if (imgNumber === '6') {
-        $('#sub-img-6').addClass('active');
-        $('#sub-img-2').removeClass('active');
-        $('#sub-img-3').removeClass('active');
-        $('#sub-img-4').removeClass('active');
-        $('#sub-img-5').removeClass('active');
-        $('#sub-img-1').removeClass('active');
-        $("#img-active").attr("src", "img/laptop-img-6.webp");
+        $('#sub-img-6').addClass('sub-img-active');
+        $('#sub-img-2').removeClass('sub-img-active');
+        $('#sub-img-3').removeClass('sub-img-active');
+        $('#sub-img-4').removeClass('sub-img-active');
+        $('#sub-img-5').removeClass('sub-img-active');
+        $('#sub-img-1').removeClass('sub-img-active');
+        $("#imageZoom").attr("src", "img/laptop-img-6.webp");
+        $('.containerZoom').css('background-image', 'url("img/laptop-img-6.webp")');
     }
 }
 // buy now button
-$('#buy-now-btn').click(function(){
-    window.location.href='./cart.html';
+$('#buy-now-btn').click(function () {
+    window.location.href = './cart.html';
 });
+
+$(function () {
+    $('#img-active').imageZoom({
+        zoom: 200
+    });
+});
+
