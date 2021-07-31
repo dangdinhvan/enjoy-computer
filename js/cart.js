a29b76cb16c06cad81253ab8ee66486440051260
dangdinhvan <untilyou1235@gmail.com>
2021-07-31 17:06:43 +0700

    ver 1.10

--- js/cart.js
+++ js/cart.js
@@ -76,6 +76,8 @@ $('#delete-all').click(function () {
     $('.delete-confirm-btn').click(function () {
         $('#content').html('Không có sản phẩm trong giỏ hàng');
         $('#content').css('justify-content', 'center').css('align-items', 'center').css('font-size', '18px');
+        $('#content').css('background-image','url(../img/empty-product.png)');
+        $('#content').css('background-repeat','no-repeat').css('background-position','center');
         fadeModalDelete();
     });
 });
